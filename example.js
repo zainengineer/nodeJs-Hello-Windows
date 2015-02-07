3045adcf704bf821c67a3116c1ad040be24831fa
zainengineer <zainengineer@gmail.com>
2015-02-07 23:30:18 +1030

    display current time

--- example.js
+++ example.js
@@ -1,9 +1,18 @@
 var http = require("http");
 
 function onRequest(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Hello World \n <br/>");
+    response.write(getCurrentDateString());
     response.end();
+}
+function getCurrentDateString(){
+    var currentDate;
+    currentDate = new Date();
+    var separator = '<b>:</b>';
+    var dateString = 'time is ' + currentDate.getHours() + separator + currentDate.getMinutes()
+       + separator + currentDate.getSeconds() + separator + currentDate.getMilliseconds();
+    return dateString;
 }
 
 http.createServer(onRequest).listen(8888);
@@ -12,3 +21,5 @@ http.createServer(onRequest).listen(8888);
 //var url = require('url');
 //var url_parts = url.parse(request.url, true);
 //var query = url_parts.query;
+
+console.log("Server has started on port 8888. Try http://localhost:8888")
